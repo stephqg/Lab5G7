43b5ec86a65093e67859c29767c8faa82d51227e
Stephany <a20151164@pucp.pe>
2020-12-17 02:27:03 -0500

    Autenticacion e ícono

--- app/src/main/java/pe/pucp/tel306/firebox/MainActivity.java
+++ app/src/main/java/pe/pucp/tel306/firebox/MainActivity.java
@@ -1,8 +1,21 @@
 package pe.pucp.tel306.firebox;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +23,50 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (currentUser != null){
+            String uid = currentUser.getUid();
+            String displayName = currentUser.getDisplayName();
+            String email = currentUser.getEmail();
+
+            Log.d("infoApp", "uid: " + uid + " | displayName: " + displayName + " | email: " + email );
+
+            //ACÁ INTENT HACIA OTRA ACTIVIDAD
+        }
+
+    }
+
+
+    public void login (View view){
+
+        List<AuthUI.IdpConfig> proveedores = Arrays.asList(
+            new AuthUI.IdpConfig.EmailBuilder().build(),
+            new AuthUI.IdpConfig.GoogleBuilder().build()
+        );
+
+        Intent intent = AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(proveedores).build();
+
+        startActivityForResult(intent, 1);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1){
+            FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+            if (currentUser != null){
+                String uid = currentUser.getUid();
+                String displayName = currentUser.getDisplayName();
+                String email = currentUser.getEmail();
+
+                Log.d("infoApp", "uid: " + uid + " | displayName: " + displayName + " | email: " + email );
+
+                //ACÁ INTENT HACIA OTRA ACTIVIDAD
+            }
+
+        }
     }
 }
