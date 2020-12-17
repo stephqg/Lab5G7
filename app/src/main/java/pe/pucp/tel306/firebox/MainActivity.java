0a6d23342ff3eb25eaf4860f05b0e3925ab4aa13
Stephany <a20151164@pucp.pe>
2020-12-17 00:32:41 -0500

    primer commit

---
+++ app/src/main/java/pe/pucp/tel306/firebox/MainActivity.java
@@ -0,0 +1,14 @@
+package pe.pucp.tel306.firebox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
