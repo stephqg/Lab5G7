26014fe1409dc02b8839a75aeb10e7885dc1686e
“José <jose.leont@pucp.pe>
2020-12-18 21:51:52 -0500

    ss

--- app/src/main/java/pe/pucp/tel306/firebox/PrincipalFragment.java
+++ app/src/main/java/pe/pucp/tel306/firebox/PrincipalFragment.java
@@ -1,21 +1,34 @@
 package pe.pucp.tel306.firebox;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import pe.pucp.tel306.firebox.Clases.Usuario;
+
 
 public class PrincipalFragment extends Fragment {
 
-
+    Usuario usuario=new Usuario();;
 
     public PrincipalFragment() {
         // Required empty public constructor
@@ -30,14 +43,27 @@ public class PrincipalFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        documento=0;
+
 
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
+        usuario=funcionesPrincipalFragmento.obtenerUsuario();
         View view = inflater.inflate(R.layout.fragment_principal, container, false);
+
+        Button btnSubir = view.findViewById(R.id.btnSubir);
+        btnSubir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              subirArchivo(view);
+            }
+        });
+
+
         return inflater.inflate(R.layout.fragment_principal, container, false);
     }
 
@@ -47,10 +73,86 @@ public class PrincipalFragment extends Fragment {
 
     }
 
+    ///////////////////////////////////////////////////////////////////////////////////
+    int documento;
+
+    //FIREBASE STORAGE
+    //SUBIR ARCHIVO AL STORAGE
+    public void subirArchivo(View view) {
+        Intent intent = new Intent();
+        intent.setType(usuario.getUid()+"/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Seleccionar archivo"), 1);
+
+    }
+
+
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        if (requestCode ==1 ){
+                subirArchivoStorage(data.getData());
+
+        }
+        if (requestCode == 1 ) {
+            subirArchivoStorage(data.getData());
+
+        }
+    }
+
+
+
+
+    private void subirArchivoStorage(Uri uri) {
+        StorageReference storageReference = FirebaseStorage.getInstance().getReference();
+        Log.d("infoApp", uri.getPath());
+
+        storageReference.child("archivo.jpg")
+                .putFile(uri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        Log.d("infoApp", "subida exitosa");
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("infoApp", "error en la subida");
+                        e.printStackTrace();
+                    }
+                });
+    }
+
+
+
+
     protected void recepcionUID(String uid, String nombre)
     {
 
 
     }
+
+
+    private FuncionesPrincipalFragmento funcionesPrincipalFragmento;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+
+        funcionesPrincipalFragmento= (FuncionesPrincipalFragmento) context;
+    }
+
+
+    public interface FuncionesPrincipalFragmento{
+
+       Usuario obtenerUsuario();
+    }
+
 
 }
