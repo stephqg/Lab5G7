02ba2fb6dd4f6948814f6c7ed72c90eaf917c01d
“José <jose.leont@pucp.pe>
2020-12-18 21:20:17 -0500

    s

--- app/src/main/java/pe/pucp/tel306/firebox/MainActivity.java
+++ app/src/main/java/pe/pucp/tel306/firebox/MainActivity.java
@@ -3,21 +3,37 @@ package pe.pucp.tel306.firebox;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 
+import com.google.android.gms.tasks.OnCanceledListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnPausedListener;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageMetadata;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
 
 import pe.pucp.tel306.firebox.Clases.Usuario;
 
@@ -43,7 +59,7 @@ public class MainActivity extends AppCompatActivity implements InicioFragment.Fu
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        documento=0;
         abrirFragmentoInicio();
 
     }
@@ -70,6 +86,8 @@ public class MainActivity extends AppCompatActivity implements InicioFragment.Fu
         f.recepcionUID(uid,displayName);
     }
 
+
+
     //FUNCION PARA BORRAR FRAGMENTO DE INICIO
     @Override
     public void borrarFragmentoInicio(String uid, String displayName, String email) {
@@ -92,6 +110,7 @@ public class MainActivity extends AppCompatActivity implements InicioFragment.Fu
 
     }
 
+
     public void abrirFragmentoPrincipal(){
 
         PrincipalFragment principalFragment = new PrincipalFragment().newInstance();
@@ -109,10 +128,36 @@ public class MainActivity extends AppCompatActivity implements InicioFragment.Fu
         if (fragment != null) {
             fragment.onActivityResult(requestCode, resultCode, data);
         }
+
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            subirArchivoStorage(data.getData());
+
+        }
+
+
     }
 
-    //FIREBASE STORAGE
+    private void subirArchivoStorage(Uri uri) {
+        StorageReference storageReference = FirebaseStorage.getInstance().getReference();
+        Log.d("infoApp", uri.getPath());
 
+        storageReference.child("archivo.jpg")
+                .putFile(uri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        Log.d("infoApp", "subida exitosa");
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("infoApp", "error en la subida");
+                        e.printStackTrace();
+                    }
+                });
+    }
 
 
     //FIRESTORE
@@ -123,7 +168,7 @@ public class MainActivity extends AppCompatActivity implements InicioFragment.Fu
 
 
         //SE crea la coleccion con el documento con el uid del usuario
-        Usuario usuario=new Usuario(nombre,correo,"FREE","500MB");
+        Usuario usuario=new Usuario(nombre,"FREE","500MB",correo);
         DocumentReference base = db.collection("User").document(uid);
         base.set(usuario).addOnSuccessListener(new OnSuccessListener<Void>() {
             @Override
@@ -139,7 +184,24 @@ public class MainActivity extends AppCompatActivity implements InicioFragment.Fu
             }
         });
 
+
     }
+///////////////////////////////////////////////////////////////////////////////////
+     int documento;
+
+    //FIREBASE STORAGE
+    //SUBIR ARCHIVO AL STORAGE
+    public void subirArchivo(View view) {
+        Intent intent = new Intent();
+        intent.setType(miUID+"/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Seleccionar archivo"), 1);
+
+    }
+
+
+
+
 
 
 }
